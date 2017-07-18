3ba81e6e6834567e6256483606577590af048a49
Dav Clark <dav@bead.glass>
2017-07-17 23:58:08 -0400

    Finished rough ADHD form

--- src/simple_forms_rum/core.clj
+++ src/simple_forms_rum/core.clj
@@ -19,22 +19,63 @@
     {question "question", [left-label right-label] "range_labels"} 
     sz-language}]
   "A simple visual-analog scale component (not a defc'd rum component, though!)"
-    (if question ; a proxy for "is there data for sz-language?"
-      [:div {:class "form-group"} 
-        [:label {:for id} question]
-        [:input {:type "range" :class "not-clicked" :id id :name (lower-case id)
-                :min 0 :max 100 :step 1}]
-        [:span {:class "ans pull-left"} left-label]
-        [:span {:class "ans pull-right"} right-label]
-      ]
-    )
+    [:div {:class "form-group"} 
+      [:label {:for id} question]
+      [:input {:type "range" :class "not-clicked" :id id :name (lower-case id)
+              :min 0 :max 100 :step 1}]
+      [:span {:class "ans pull-left"} left-label]
+      [:span {:class "ans pull-right"} right-label]
+    ]
   )
+
+(defmethod sz-item "instructions"
+  [{id "id"
+    {instructions "instructions"} sz-language}]
+  "A simple div with potentially multi-line instructions"
+  [:div instructions]
+)
+
+(defmethod sz-item "heading"
+  [{id "id"
+    {heading "heading"} sz-language}]
+  "A simple div with potentially multi-line instructions"
+  [:h2 heading]
+)
+
+(defmethod sz-item "likert5"
+  [{id "id"
+    {question "question"} sz-language}]
+  "A 5-point likert scale as used in the ADHD self-report scale"
+  (let [choices (
+        {"en" ["Never" "Rarely" "Sometimes" "Often" "Very Often"],
+         "jp" ["全く" "稀に" "たまに" "しばしば" "頻繁に"]} sz-language)]
+
+        ; First we create the static part of our div
+        [:div {:class "form-group"} 
+          ; Radio buttons have an unusual label approach, so we make our true
+          ; label a span for now
+          [:div question]
+          [:div {:class "row text-center"}
+          ; and then we add our choices dynamically
+          (into [:div {:class "btn-group" :data-toggle "buttons"}]
+            (map-indexed
+              (fn [num choice]
+                ; For now we're using a bootstrap style
+                [:label {:for (str id "-" num) :class "btn btn-primary"}
+                  [:input {:type "radio" :id (str id "-" num) :value num :name (lower-case id) 
+                            :autocomplete "off"}]
+                  choice]
+              ) 
+              choices) ) ] ]
+  ))
+
+(-main "adhd")
 
 (rum/defc sz-form [items]
   "Create a form with some divs"
   ; For now, our containing form tag is in the HTML template
   [:div ;:form
-    (keep sz-item items)
+    (keep #(if (% sz-language) (sz-item %)) items)
   ] )
 
 (defn -main
