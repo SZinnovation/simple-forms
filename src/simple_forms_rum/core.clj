6b46e001eac01c3144380bdbe1fdb589e33a2e88
Dav Clark <dav@bead.glass>
2017-07-17 20:58:25 -0400

    Fully working form generation to a file

--- src/simple_forms_rum/core.clj
+++ src/simple_forms_rum/core.clj
@@ -4,48 +4,48 @@
             [clojure.string :refer [lower-case]])
   (:gen-class))
 
+
+; should ultimately find a better way to configure the language
+; Maybe just load from a config file
+(def sz-language "jp")
+
 ; This is a little clunky because we're not using keyword maps
+; But we're still dispatching on type
 (defmulti sz-item #(% "type"))
 
 (defmethod sz-item "vas" 
   [{id "id" 
-  ; This documents the expected form-text map
-  ; Also, we should find a way to configure the language
-   {question "question", [left-label right-label] "range_labels"} "jp"}]
+    ; This documents the expected contents of the map
+    {question "question", [left-label right-label] "range_labels"} 
+    sz-language}]
   "A simple visual-analog scale component (not a defc'd rum component, though!)"
+    (if question ; a proxy for "is there data for sz-language?"
       [:div {:class "form-group"} 
         [:label {:for id} question]
         [:input {:type "range" :class "not-clicked" :id id :name (lower-case id)
-                 :min 0 :max 100 :step 1}]
+                :min 0 :max 100 :step 1}]
         [:span {:class "ans pull-left"} left-label]
         [:span {:class "ans pull-right"} right-label]
       ]
+    )
   )
 
 (rum/defc sz-form [items]
   "Create a form with some divs"
-  [:form
-    ; Convert this to a keep over a multi-method that dispatches on "type"
+  ; For now, our containing form tag is in the HTML template
+  [:div ;:form
     (keep sz-item items)
-    ; (for [item items
-    ;         ; At some point we should make language configurable
-    ;         :let [{form-text "jp", id "id"} item]
-    ;       ; Right now, we're not guaranteed to have items in all languages
-    ;       ; The sensible approach for the demo is to drop those
-    ;       :when form-text]
-    ;       (vas-input id form-text) )
   ] )
 
 (defn -main
   "Convert a YAML specification into an HTML form"
-  [& args]
-  (let [vas-gva (yaml/from-file "form-data/vas-gva.yaml")
-        vas-dom (sz-form vas-gva)]
-    (println (rum/render-static-markup vas-dom))
-    )
-  )
+  [form-name & args]
+  (let [form-items (yaml/from-file (format "form-data/%s.yaml" form-name))
+        form-html (sz-form form-items)
+        template (slurp "form-data/form-template.html")]
+    (spit (format "rendered/%s.html" form-name)
+      (format template 
+        (rum/render-static-markup form-html) ))
+  ))
 
-(-main)
-
-; This gets our working directory
-; (System/getProperty "user.dir")
+; (-main "vas-gva")
